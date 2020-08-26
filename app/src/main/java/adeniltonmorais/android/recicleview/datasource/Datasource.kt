b40bbbad81abba209357763ac5fe69a54228a59b
Adeniltonarcanjo <adeniltonmorais@gmail.com>
2020-08-26 13:57:57 -0300

    aula 2 app lista de jogos, Professor Celso Furtado

--- app/src/main/java/adeniltonmorais/android/recicleview/datasource/Datasource.kt
+++ app/src/main/java/adeniltonmorais/android/recicleview/datasource/Datasource.kt
@@ -2,14 +2,22 @@ package adeniltonmorais.android.recicleview.datasource
 
 import adeniltonmorais.android.recicleview.model.Jogo
 
+
 class Datasource {
 
     companion object {
 
         fun getJogos():ArrayList<Jogo> {
             var jogos=ArrayList<Jogo>()
-            jogos.add(Jogo(100,"Resident Eviel",5,"ps4"))
+            jogos.add(Jogo(130,"Mario Kart",4,"pc"))
+            jogos.add(Jogo(100,"Resident Evil",5,"ps4"))
             jogos.add(Jogo(200,"Zobier ARme",4,"pc"))
+            jogos.add(Jogo(229,"crach",5,"ps2"))
+            jogos.add(Jogo(214,"mario",4,"ps1"))
+            jogos.add(Jogo(110,"Tob Raider",3,"ps"))
+            jogos.add(Jogo(105,"race car",5,"pc"))
+            jogos.add(Jogo(106,"sonic",4,"ps2"))
+
             return jogos
         }
 
