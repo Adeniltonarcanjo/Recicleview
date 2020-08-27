f409418bdbc7342d5a9da0b688341141f300ebe1
Adeniltonarcanjo <adeniltonmorais@gmail.com>
2020-08-26 21:39:07 -0300

    aula 12 app lista de jogos, Professor Celso Furtado

--- app/src/main/java/adeniltonmorais/android/recicleview/UI/CadastroJogoActivity.kt
+++ app/src/main/java/adeniltonmorais/android/recicleview/UI/CadastroJogoActivity.kt
@@ -3,12 +3,63 @@ package adeniltonmorais.android.recicleview.UI
 import adeniltonmorais.android.recicleview.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_cadastro_jogo.*
+import kotlinx.android.synthetic.main.toolbar.*
+
 
 class CadastroJogoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_jogo)
 
-
+        insertToolbar()
     }
+
+    private fun insertToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title="Novo Jogo"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true) // botão de voltar no actionBar do layout de cadastro
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_game,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_cancelar -> {
+                alert()
+            }
+            R.id.menu_salvar -> {
+                Toast.makeText(this, "Salvar", Toast.LENGTH_SHORT)
+            }
+            else -> {onBackPressed()}
+
+
+        }
+        return true
+    }
+
+    private fun alert() {
+       var builderDialog=AlertDialog.Builder(this)
+        builderDialog.setTitle("Cancelar Cadastro")
+        builderDialog.setMessage("Tem certeza que deseja cancelar o cadastro do seu jogo?")
+        builderDialog.setIcon(R.drawable.ic_baseline_help_outline_red_24)
+
+        builderDialog.setPositiveButton("Sim"){_,_-> onBackPressed()
+
+        }
+        builderDialog.setNegativeButton("Não"){_,_ ->
+            editTextNomeDoJogo.requestFocus()
+        }
+
+        builderDialog.show()
+    }
+
+
 }
--- app/src/main/java/adeniltonmorais/android/recicleview/UI/MainActivity.kt
+++ app/src/main/java/adeniltonmorais/android/recicleview/UI/MainActivity.kt
@@ -3,20 +3,30 @@ package adeniltonmorais.android.recicleview.UI
 import adeniltonmorais.android.recicleview.R
 import adeniltonmorais.android.recicleview.adapter.JogosAdapter
 import adeniltonmorais.android.recicleview.datasource.Datasource
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.toolbar.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         iniciarRecyclerView()
+        buttonCadastrarJogo.setOnClickListener (this)
 
+        insertToolbar()
 
+    }
+
+    private fun insertToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title="My Game App"
     }
 
     private fun iniciarRecyclerView(){
@@ -25,6 +35,14 @@ class MainActivity : AppCompatActivity() {
        recyclerviewJogos.adapter= JogosAdapter(Datasource.getJogos() )
 
 
+    }
+
+    override fun onClick(v: View) {
+
+       if(v.id==R.id.buttonCadastrarJogo){
+           val intent= Intent(this,CadastroJogoActivity::class.java)
+           startActivity(intent)
+       }
     }
 
 
